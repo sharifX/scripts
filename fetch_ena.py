6cd211b7e4de003c57356183d2880e56a8a4e156
Sharif Islam <sharif.islam@naturalis.nl>
2020-03-11 10:22:32 +0100

    add sequence_md5

--- fetch_ena.py
+++ fetch_ena.py
@@ -9,10 +9,12 @@ headers = {
 data = {
   'result': 'sequence_release',
   'query': 'specimen_voucher="RMNH*"',
-  'fields': 'scientific_name,specimen_voucher,country,accession,tax_id',
+  'fields': 'scientific_name,specimen_voucher,accession,tax_id,last_updated,sequence_md5,country',
   'format': 'json',
   'limit': '2'
 }
+
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "result=sequence_release&query=specimen_voucher%3D%22RMNH*%22&fields=specimen_voucher%2Cscientific_name%2Caccession%2Ctax_id%2Ccollected_by%2Clast_updated%2Chost%2Clab_host%2Cdescription%2Ccollection_date&format=tsv" "https://www.ebi.ac.uk/ena/portal/api/search"
 
 
 def jprint(obj):
@@ -25,7 +27,7 @@ response = requests.post('https://www.ebi.ac.uk/ena/portal/api/search', headers=
 
 raw = response.json()
 
-for item in raw: 
-    item['type'] = "Digital Specimen"
+#for item in raw: 
+#    item['type'] = "Digital Specimen"
 
 jprint(raw)
